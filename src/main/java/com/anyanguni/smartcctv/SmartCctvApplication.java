1e75ebb3a7ab661ff3932cd5052d073d28a2717a
PC1 <dlagksrb0430@naver.com>
2024-08-05 19:12:25 +0900

    Initial commit

---
+++ src/main/java/com/anyanguni/smartcctv/SmartCctvApplication.java
@@ -0,0 +1,13 @@
+package com.anyanguni.smartcctv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCctvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCctvApplication.class, args);
+	}
+
+}
