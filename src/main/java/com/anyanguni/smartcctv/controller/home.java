9f6b1418097a4cb3b1bc6af49adb416357dcc718
PC1 <dlagksrb0430@naver.com>
2024-09-08 01:03:06 +0900

    기초 작업

--- src/main/java/com/anyanguni/smartcctv/controller/home.java
+++ src/main/java/com/anyanguni/smartcctv/controller/home.java
@@ -1,10 +1,15 @@
 package com.anyanguni.smartcctv.controller;
 
 import ch.qos.logback.core.model.Model;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class home {
-
+    @GetMapping("/")
+    public String home(){
+        return "member/login";
+    }
 }
